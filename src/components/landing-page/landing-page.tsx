2cff7f68025d4505bb58eba1355f03251978f8ad
Colin Bruin <colinjb23@gmail.com>
2023-01-14 12:49:12 -0700

    hiding comment

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -2,7 +2,7 @@ import './landing-page.css';
 export default function LandingPage() {
   return (
     <div>
-      // Background photo here
+      {/* Background photo here */}
       <div id='landing-main-photo'>
         <h1 id='welcome-message'>We are here to connect you</h1>
       </div>
