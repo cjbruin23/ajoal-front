0b2913bcfaa9583f960b7d46427d405a8026c0e1
Colin Bruin <colinjb23@gmail.com>
2023-01-14 11:39:22 -0700

    removing unecessary code and asset

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import AppBar from './components/appBar'
 
 function App() {
 
   return (
-    <div className="App">
-      
+    <div>
+      <AppBar></AppBar>
     </div>
   )
 }
