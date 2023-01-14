3327d2ced93c0472bee650fc6ba8a0ecec798401
Colin Bruin <colinjb23@gmail.com>
2023-01-14 12:48:40 -0700

    centering welcome message

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -1,7 +1,11 @@
+import './landing-page.css';
 export default function LandingPage() {
   return (
     <div>
-      <h1>We are here to connect you</h1>
+      // Background photo here
+      <div id='landing-main-photo'>
+        <h1 id='welcome-message'>We are here to connect you</h1>
+      </div>
     </div>
   );
 }
