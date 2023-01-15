130e3be1bc34e0b22b4fb474ef40490d404a2d57
Colin Bruin <colinjb23@gmail.com>
2023-01-15 13:39:01 -0700

    adding auth0 dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain='pareate.us.auth0.com'
+      clientId='UWrdsA1ydeI8Wdou4YL7MlI0wZobtt2v'
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
-)
+);
