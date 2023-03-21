a889baa5f15925aab7066965bda7edaa81d72fdc
Colin Bruin <colinjb23@gmail.com>
2023-03-21 07:49:58 -0700

    getting rid of boilerplate code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,9 +37,5 @@
   };
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
 <button on:click={login}> Login </button>
 <button on:click={logout}> Logout </button>
