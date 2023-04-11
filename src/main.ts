0d1d9a5f2c4efb1f7ecdc019c97b816d6d0cfb8b
Colin Bruin <colinjb23@gmail.com>
2023-04-11 13:32:38 -0700

    login button added, files removed, css cleansed

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,19 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import { createAuth0 } from '@auth0/auth0-vue'
 
 const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+app
+  .use(router)
+  .use(
+    createAuth0({
+      domain: import.meta.env.VITE_AUTH0_DOMAIN,
+      clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+      authorizationParams: {
+        redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL
+      }
+    })
+  )
+  .mount('#app')
