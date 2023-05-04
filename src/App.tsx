0314e5260a1523a19b9aeb8c92c1293039b50aba
Colin Bruin <colinjb23@gmail.com>
2023-05-04 13:53:30 -0700

    adding in axios and sending user data to the backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import MainAppBar from "./components/AppBar";
 import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
+
+interface User {
+  userId: string;
+  nickname: string;
+}
 
 function App() {
   const { isAuthenticated, user } = useAuth0();
   console.log("isAuthenticated", isAuthenticated);
-  console.log("use", user);
+  useEffect(() => {
+    if (isAuthenticated) {
+      console.log("make api call here");
+      console.log("use", user);
+      const userData = { userId: user?.sub, nickname: user?.nickname } as User;
+      axios({
+        url: "http://localhost:8000/user",
+        method: "POST",
+        data: userData,
+      });
+    }
+  }, [isAuthenticated]);
   return (
     <div>
       <MainAppBar></MainAppBar>
