a405f746c6e250176846e665d5a0636ac3f82e6b
Colin Bruin <colinjb23@gmail.com>
2023-07-22 11:50:08 -0700

    cleanup of the hamburger menu. I do not need it right now

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,21 +1,10 @@
-import { IconButton, Toolbar, Typography, Button } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { Toolbar, Typography } from "@mui/material";
 import LoginButton from "./LoginButton/LoginButton";
 import LogoutButton from "./LogoutButton";
 
 const MainAppBar = () => {
   return (
     <Toolbar>
-      <IconButton
-        size="large"
-        edge="start"
-        color="inherit"
-        aria-label="menu"
-        sx={{ mr: 2 }}
-      >
-        <MenuIcon />
-      </IconButton>
-
       <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
         <a href="/">Periate</a>
       </Typography>
