4f74d9bf83c58ee47fa3e0362fb0c6004d52d8e1
cjbruin23 <colinjb23@gmail.com>
2023-08-04 12:21:57 -0700

    adding initial form

--- src/components/QuestionInputForm/QuestionInputForm.tsx
+++ src/components/QuestionInputForm/QuestionInputForm.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 const QuestionInputForm = () => {
-  return <div>Question Input Form</div>;
+  const [titleValue, setTitleValue] = useState('');
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitleValue(event.target.value)
+  }
+
+  return <div>
+    <form>
+      <label className='ml-1'>Title:
+      <input type="text" value={titleValue} onChange={handleTitleChange} className='ml-1'></input>
+      </label>
+    </form>
+    </div>;
 };
 
 export default QuestionInputForm;
