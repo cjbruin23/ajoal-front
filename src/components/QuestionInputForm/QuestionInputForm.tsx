8918c5bd76360898467b1000a805e178601517b7
cjbruin23 <colinjb23@gmail.com>
2023-08-29 04:46:49 -0700

    added a text area form field, handle the initial submit event

--- src/components/QuestionInputForm/QuestionInputForm.tsx
+++ src/components/QuestionInputForm/QuestionInputForm.tsx
@@ -2,16 +2,36 @@ import { useState } from "react";
 
 const QuestionInputForm = () => {
   const [titleValue, setTitleValue] = useState('');
+  const [bodyValue, setBodyValue] = useState('')
 
   const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTitleValue(event.target.value)
+    console.log(event.target.name)
+    const fieldName = event.target.name;
+    switch (fieldName) {
+      case 'title':
+        setTitleValue(event.target.value);
+      case 'body':
+        setBodyValue(event.target.value)
+    }
+  }
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const formData = {'title': titleValue, 'body': bodyValue}
+    console.log('formData', formData)
   }
 
   return <div>
-    <form>
+    <form onSubmit={handleSubmit}>
       <label className='ml-1'>Title:
-      <input type="text" value={titleValue} onChange={handleTitleChange} className='ml-1'></input>
+      <input type="text" name='title' value={titleValue} onChange={handleTitleChange} className='ml-1'></input>
       </label>
+
+      <label className='ml-1'>Body:
+      <input type='textarea' name='body' value={bodyValue}  onChange={handleTitleChange} className='ml-1'></input>
+      </label>
+
+      <button type='submit'>Submit</button>
     </form>
     </div>;
 };
