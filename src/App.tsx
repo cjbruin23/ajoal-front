9776592ab34444c5ecccb44e7f3234be03072265
cjbruin23 <colinjb23@gmail.com>
2023-12-19 12:54:58 -0800

    adding auth0 client library for login and user information

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
+import { createAuth0Client } from "@auth0/auth0-spa-js";
 import "./App.css";
+
+const login = () => {
+  console.log("login");
+  console.log('clientId', import.meta.env.VITE_CLIENT_ID);
+  const configureClient = async () => {
+    const auth0Client = await createAuth0Client({
+      domain: import.meta.env.VITE_AUTH0_DOMAIN,
+      clientId: import.meta.env.VITE_CLIENT_ID
+    });
+    const userAuthenticated = await auth0Client.isAuthenticated();
+    console.log('userAuthenticated', userAuthenticated)
+  }
+
+  configureClient();
+};
+
+const logout = () => {
+  console.log("logout");
+};
 
 function App() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Periate
-          </a>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <h2>SPA Authentication Sample</h2>
+      <p>Welcome to our page!</p>
+      <button id="btn-login" onClick={login}>
+        Log in
+      </button>
+      <button id="btn-logout" onClick={logout}>
+        Log out
+      </button>
     </>
   );
 }
