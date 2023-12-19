6423a9e29bcb2a8db4c8e18935b3712420d6b092
cjbruin23 <colinjb23@gmail.com>
2023-12-19 12:05:42 -0800

    adding bootstrap to solid js project

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,28 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect } from "react";
-import MainAppBar from "./components/AppBar/AppBar";
-import QuestionHome from "./components/QuestionHome/QuestionHome";
-import axios from "axios";
-import WelcomeBody from "./components/WelcomeBody/WelcomeBody";
-import "./App.css";
+import './App.css'
 
-interface User {
-  authId: string;
-  nickname: string;
-}
 function App() {
-  const { isAuthenticated, user } = useAuth0();
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      const userData = { authId: user?.sub, nickname: user?.nickname } as User;
-      axios({
-        url: "http://localhost:8000/users",
-        method: "POST",
-        data: userData,
-      });
-    }
-  }, [isAuthenticated]);
 
   return (
-      <div>
-        <MainAppBar></MainAppBar>
-        {!isAuthenticated ? (
-          <WelcomeBody></WelcomeBody>
-        ) : (
-          <QuestionHome></QuestionHome>
-        )}
-      </div>
-  );
+    <>
+      <h1>Hello World</h1>
+      <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1"></input>
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/components/QuestionHome/QuestionHome.tsx
+++
@@ -1,7 +0,0 @@
-import QuestionInputForm from "../QuestionInputForm/QuestionInputForm";
-
-const QuestionHome = () => {
-  return <QuestionInputForm></QuestionInputForm>;
-};
-
-export default QuestionHome;
