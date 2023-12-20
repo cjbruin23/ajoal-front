3204c7c92b2a76a697e121d4df7ad113cf73c364
cjbruin23 <colinjb23@gmail.com>
2023-12-20 10:44:41 -0800

    needing to switch to loginWithPopup instead of loginWithRedirect; loginwithRedirect is not allowing me to login after I clear the cookies. Will investigate further

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { createAuth0Client } from "@auth0/auth0-spa-js";
+import { Auth0Client, createAuth0Client } from "@auth0/auth0-spa-js";
 import "./App.css";
 
-const login = () => {
-  console.log("login");
-  console.log('clientId', import.meta.env.VITE_CLIENT_ID);
-  const configureClient = async () => {
-    const auth0Client = await createAuth0Client({
-      domain: import.meta.env.VITE_AUTH0_DOMAIN,
-      clientId: import.meta.env.VITE_CLIENT_ID
-    });
-    const userAuthenticated = await auth0Client.isAuthenticated();
-    console.log('userAuthenticated', userAuthenticated)
-  }
+let auth0Client: Auth0Client;
 
-  configureClient();
+const configureClient = async () => {
+  auth0Client = await createAuth0Client({
+    domain: import.meta.env.VITE_AUTH0_DOMAIN,
+    clientId: import.meta.env.VITE_CLIENT_ID,
+  });
+};
+
+const login = async () => {
+  console.log("login");
+  console.log("clientId", import.meta.env.VITE_CLIENT_ID);
+  await auth0Client.loginWithPopup();
+  const userAuthenticated = await auth0Client.isAuthenticated();
+  console.log("userAuthenticated", userAuthenticated);
+  console.log("user", await auth0Client.getUser());
 };
 
 const logout = () => {
@@ -21,6 +24,8 @@ const logout = () => {
 };
 
 function App() {
+  configureClient();
+
   return (
     <>
       <h2>SPA Authentication Sample</h2>
