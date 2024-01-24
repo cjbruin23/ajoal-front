97f747e9b3b85fc85b8dc8643dc51ba398e5cff9
cjbruin23 <colinjb23@gmail.com>
2024-01-24 14:46:17 -0800

    Revert "adding tailwindcss"
    This reverts commit f32945ea7b3d26f132335f088f085a1cb95f636e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css'
-import Header from './components/header/Header'
 
 function App() {
 
   return (
-    <div>
-    <Header></Header>
-    <h1 className="">
-      Welcome to Periate!
-    </h1>
-    </div>
+    <>
+    <div>Welcome!</div>
+    </>
   )
 }
 
