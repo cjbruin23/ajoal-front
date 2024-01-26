71111783c1cab6bc512d8be4a7728ccfb85ed027
cjbruin23 <colinjb23@gmail.com>
2024-01-26 12:11:42 -0800

    adding a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import './App.css'
-import Header from './components/header/Header'
+import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
-
   return (
     <div>
-    <Header></Header>
-    <h1 className="">
-      Welcome to Periate!
-    </h1>
+      <Header></Header>
+      <h1>Welcome to Ajoal!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
